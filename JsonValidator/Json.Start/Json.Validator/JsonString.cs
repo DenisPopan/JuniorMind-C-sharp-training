ca00f6954fa560daa6881c5235df05d906e66247
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-02 22:18:53 +0300

    Checked if string is double quoted

--- JsonValidator/Json.Start/Json.Validator/JsonString.cs
+++ JsonValidator/Json.Start/Json.Validator/JsonString.cs
@@ -6,7 +6,19 @@ namespace Json
     {
         public static bool IsJsonString(string input)
         {
-            throw new NotImplementedException();
+            return HasContent(input)
+                && IsDoubleQuoted(input);
+        }
+
+        private static bool IsDoubleQuoted(string input)
+        {
+            const byte minimumLength = 2;
+            return input.Length >= minimumLength && input[0] == '"' && input[input.Length - 1] == '"';
+        }
+
+        private static bool HasContent(string input)
+        {
+            return !string.IsNullOrEmpty(input);
         }
     }
 }
