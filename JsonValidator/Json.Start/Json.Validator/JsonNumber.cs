90c3b6dc99e498aa170d1159336a095992b576f9
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-07 10:53:39 +0300

    Checked if number contains letters - Refactor

--- JsonValidator/Json.Start/Json.Validator/JsonNumber.cs
+++ JsonValidator/Json.Start/Json.Validator/JsonNumber.cs
@@ -6,13 +6,26 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            return input.IndexOf('0') != -1
+            return IsZero(input)
                 && !ContainsLetters(input);
         }
 
         private static bool ContainsLetters(string input)
         {
+            foreach (char character in input)
+            {
+                if (char.IsLetter(character))
+                {
+                    return true;
+                }
+            }
+
             return false;
+        }
+
+        private static bool IsZero(string input)
+        {
+            return input.IndexOf('0') != -1;
         }
     }
 }
