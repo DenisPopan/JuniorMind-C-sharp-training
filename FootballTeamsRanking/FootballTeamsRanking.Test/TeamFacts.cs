17898b3f257b8155d1de335b24b00b6d026f7aa2
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-14 15:45:18 +0300

    Added the new project, a new class and its facts

---
+++ FootballTeamsRanking/FootballTeamsRanking.Test/TeamFacts.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using static FootballTeamsRanking.Team;
+
+namespace FootballTeamsRanking.Test
+{
+    public class TeamFacts
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ FootballTeamsRanking/FootballTeamsRanking/Team.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace FootballTeamsRanking
+{
+    public class Team
+    {
+
+    }
+}
