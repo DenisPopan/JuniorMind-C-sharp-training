ddce1083bbeeb1c61680ee1904e9326c3a44a523
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-16 23:01:28 +0300

    Checked if ranking is updated correctly after many matches

--- FootballTeamsRanking/FootballTeamsRanking.Test/RankingFacts.cs
+++ FootballTeamsRanking/FootballTeamsRanking.Test/RankingFacts.cs
@@ -150,5 +150,32 @@ namespace FootballTeamsRanking.Test
             Assert.Equal(4, ranking.TeamPosition(team2));
             Assert.Equal(3, ranking.TeamPosition(team5));
         }
+
+        [Fact]
+        public void RankingIsUpdatedCorrectlyAfterManyMatches()
+        {
+            Team team1 = new Team("Barcelona", 12);
+            Team team2 = new Team("Real Madrid", 24);
+            Team team3 = new Team("Bayern Munchen", 16);
+            Team team4 = new Team("Argentina", 30);
+            Team team5 = new Team("Brazil", 28);
+            Team team6 = new Team("Romania", 21);
+            Team team7 = new Team("Portugal", 31);
+            Team[] teams = { team7, team4, team5, team2, team6, team3, team1 };
+            Ranking ranking = new Ranking(teams);
+            ranking.UpdateRanking(team1, team5, 6, 3);
+            ranking.UpdateRanking(team2, team4, 3, 5);
+            ranking.UpdateRanking(team3, team6, 4, 4);
+            ranking.UpdateRanking(team7, team4, 6, 7);
+            ranking.UpdateRanking(team5, team1, 1, 0);
+            ranking.UpdateRanking(team3, team7, 8, 1);
+            Assert.Equal(1, ranking.TeamPosition(team4));
+            Assert.Equal(2, ranking.TeamPosition(team5));
+            Assert.Equal(3, ranking.TeamPosition(team3));
+            Assert.Equal(4, ranking.TeamPosition(team7));
+            Assert.Equal(5, ranking.TeamPosition(team2));
+            Assert.Equal(6, ranking.TeamPosition(team6));
+            Assert.Equal(7, ranking.TeamPosition(team1));
+        }
     }
 }
