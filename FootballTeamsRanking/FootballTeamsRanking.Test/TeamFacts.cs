cda4e643533e660fa1891e7fee8e53e182cd5a41
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-17 16:10:03 +0300

    Checked if CompareNames method compares teams names correctly

--- FootballTeamsRanking/FootballTeamsRanking.Test/TeamFacts.cs
+++ FootballTeamsRanking/FootballTeamsRanking.Test/TeamFacts.cs
@@ -19,5 +19,23 @@ namespace FootballTeamsRanking.Test
             Assert.Equal(0, team4.ComparePoints(team2));
             Assert.Equal(0, team5.ComparePoints(team6));
         }
+
+        [Fact]
+        public void CompareNamesMethodShouldCompareTeamsNamesCorrectly()
+        {
+            Team team1 = new Team("Spain", 26);
+            Team team2 = new Team("Czech Republic", 18);
+            Team team3 = new Team("Serbia", 30);
+            Team team4 = new Team("Hungary", 18);
+            Team team5 = new Team("Geramny", 0);
+            Team team6 = new Team("Portugal", 0);
+            Team team7 = new Team("Portugal", 0);
+
+            Assert.Equal(1, team1.CompareNames(team2));
+            Assert.Equal(-1, team2.CompareNames(team3));
+            Assert.Equal(1, team4.CompareNames(team2));
+            Assert.Equal(-1, team5.CompareNames(team6));
+            Assert.Equal(0, team6.CompareNames(team7));
+        }
     }
 }
