8eb1e918a5ee9125b3ff9be318f226bae7bfa06a
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-19 17:33:06 +0300

    Checked if Match method tests the first char from a string to be in a range

--- JsonValidator/Json.Start/Json.Facts/RangeFacts.cs
+++ JsonValidator/Json.Start/Json.Facts/RangeFacts.cs
@@ -2,8 +2,20 @@
 
 namespace Json.Facts
 {
-    class RangeFacts
+    public class RangeFacts
     {
-
+        [Fact]
+        public void MatchMethodShouldReturnIfTheFirstCharacterFromAStringIsInRange()
+        {
+            var range = new Range('a', 'f');
+            string text1 = "abc";
+            string text2 = "fab";
+            string text3 = "bcd";
+            string text4 = "1ab";
+            Assert.True(range.Match(text1));
+            Assert.True(range.Match(text2));
+            Assert.True(range.Match(text3));
+            Assert.False(range.Match(text4));
+        }
     }
 }
--- JsonValidator/Json.Start/Json.Validator/Range.cs
+++ JsonValidator/Json.Start/Json.Validator/Range.cs
@@ -2,17 +2,20 @@
 
 namespace Json
 {
-    class Range
+    public class Range
     {
+        readonly char start;
+        readonly char end;
 
         public Range(char start, char end)
         {
-
+            this.start = start;
+            this.end = end;
         }
 
         public bool Match(string text)
         {
-
+            return text[0] >= start && text[0] <= end;
         }
     }
 }
