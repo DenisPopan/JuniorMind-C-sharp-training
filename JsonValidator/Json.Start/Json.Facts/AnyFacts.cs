9f9fd679b43d05f3399f79a7beb3faa3b09e065d
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-23 13:17:24 +0300

    Added any class
    And checked if its match method returns if first char of a given string is part of a string received through the constructor

---
+++ JsonValidator/Json.Start/Json.Facts/AnyFacts.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace Json.Facts
+{
+    public class AnyFacts
+    {
+        [Fact]
+        public void AnyClassShouldReturnIfFirstCharOfAGivenStringIsPartOfAConstructorString()
+        {
+            var e = new Any("eE");
+            IMatch match1 = e.Match("ea");
+            IMatch match2 = e.Match("Ea");
+            IMatch match3 = e.Match("a");
+            IMatch match4 = e.Match("");
+            IMatch match5 = e.Match(null);
+
+            Assert.Equal((true, "a"), (match1.Success(), match1.RemainingText()));
+            Assert.Equal((true, "a"), (match2.Success(), match2.RemainingText()));
+            Assert.Equal((false, "a"), (match3.Success(), match3.RemainingText()));
+            Assert.Equal((false, ""), (match4.Success(), match4.RemainingText()));
+            Assert.Equal((false, null), (match5.Success(), match5.RemainingText()));
+        }
+    }
+}
---
+++ JsonValidator/Json.Start/Json.Validator/Any.cs
@@ -0,0 +1,19 @@
+﻿namespace Json
+{
+    public class Any : IPattern
+    {
+        readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            return !string.IsNullOrEmpty(text) && accepted.IndexOf(text[0]) != -1
+                ? new Match(text.Substring(1), true)
+                : new Match(text, false);
+        }
+    }
+}
