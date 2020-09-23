23232334348ccb941d73fe21cd3cdc8555f3c687
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-23 13:23:05 +0300

    Checked if Any class returns if first char is a sign

--- JsonValidator/Json.Start/Json.Facts/AnyFacts.cs
+++ JsonValidator/Json.Start/Json.Facts/AnyFacts.cs
@@ -20,5 +20,22 @@ namespace Json.Facts
             Assert.Equal((false, ""), (match4.Success(), match4.RemainingText()));
             Assert.Equal((false, null), (match5.Success(), match5.RemainingText()));
         }
+
+        [Fact]
+        public void AnyClassShouldReturnIfFirstCharOfAGivenTextIsASign()
+        {
+            var sign = new Any("-+");
+            IMatch match1 = sign.Match("+3");
+            IMatch match2 = sign.Match("-2");
+            IMatch match3 = sign.Match("2");
+            IMatch match4 = sign.Match("");
+            IMatch match5 = sign.Match(null);
+
+            Assert.Equal((true, "3"), (match1.Success(), match1.RemainingText()));
+            Assert.Equal((true, "2"), (match2.Success(), match2.RemainingText()));
+            Assert.Equal((false, "2"), (match3.Success(), match3.RemainingText()));
+            Assert.Equal((false, ""), (match4.Success(), match4.RemainingText()));
+            Assert.Equal((false, null), (match5.Success(), match5.RemainingText()));
+        }
     }
 }
