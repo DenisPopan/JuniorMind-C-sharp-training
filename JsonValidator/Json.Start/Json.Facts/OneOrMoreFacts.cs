56bcff54711f71de69ceb5db1ac8063cba4e13ef
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-24 20:26:34 +0300

    Added OneOrMore Class and its facts

---
+++ JsonValidator/Json.Start/Json.Facts/OneOrMoreFacts.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Json.Facts
+{
+    public class OneOrMoreFacts
+    {
+        [Fact]
+        public void asfsaf()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            IMatch match1 = a.Match("123");
+            IMatch match2 = a.Match("1a"); 
+            IMatch match3 = a.Match("bc");
+            IMatch match4 = a.Match("");
+            IMatch match5 = a.Match(null);
+
+            Assert.Equal((true, ""), (match1.Success(), match1.RemainingText()));
+            Assert.Equal((true, "a"), (match2.Success(), match2.RemainingText()));
+            Assert.Equal((false, "bc"), (match3.Success(), match3.RemainingText()));
+            Assert.Equal((false, ""), (match4.Success(), match4.RemainingText()));
+            Assert.Equal((false, null), (match5.Success(), match5.RemainingText()));
+
+        }
+    }
+}
---
+++ JsonValidator/Json.Start/Json.Validator/OneOrMore.cs
@@ -0,0 +1,17 @@
+﻿namespace Json
+{
+    public class OneOrMore
+    {
+        private readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = new Sequence(new Choice(pattern), new Many(pattern));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
