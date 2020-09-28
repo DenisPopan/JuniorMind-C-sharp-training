554f1c5a12ff6d9f2a0be1c4021ab81b1b5638bd
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-28 13:01:13 +0300

    Added class Number and its facts

--- JsonValidator/Json.Start/Json.Facts/JsonNumberFacts.cs
+++ JsonValidator/Json.Start/Json.Facts/JsonNumberFacts.cs
@@ -9,140 +9,164 @@ namespace Json.Facts
         [Fact]
         public void CanBeZero()
         {
-            Assert.True(IsJsonNumber("0"));
+            var number = new Number().Match("0");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void DoesNotContainLetters()
         {
-            Assert.False(IsJsonNumber("a"));
+            var number = new Number().Match("a");
+            Assert.Equal((false, "a"), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void CanHaveASingleDigit()
         {
-            Assert.True(IsJsonNumber("7"));
+            var number = new Number().Match("7");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void CanHaveMultipleDigits()
         {
-            Assert.True(IsJsonNumber("70"));
+            var number = new Number().Match("70");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void DoesNotStartWithALetter()
         {
-            Assert.False(IsJsonNumber("a820"));
+            var number = new Number().Match("a820");
+            Assert.Equal((false, "a820"), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void IsNotNull()
         {
-            Assert.False(IsJsonNumber(null));
+            var number = new Number().Match(null);
+            Assert.Equal((false, null), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void IsNotAnEmptyString()
         {
-            Assert.False(IsJsonNumber(string.Empty));
+            var number = new Number().Match(string.Empty);
+            Assert.Equal((false, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void DoesNotStartWithZero()
         {
-            Assert.False(IsJsonNumber("07"));
+            var number = new Number().Match("07");
+            Assert.Equal((true, "7"), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void CanBeNegative()
         {
-            Assert.True(IsJsonNumber("-26"));
+            var number = new Number().Match("-26");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void CanBeMinusZero()
         {
-            Assert.True(IsJsonNumber("-0"));
+            var number = new Number().Match("-0");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void CannotBeMinusZeroZero()
         {
-            Assert.False(IsJsonNumber("-00"));
+            var number = new Number().Match("-00");
+            Assert.Equal((true, "0"), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void CanBeFractional()
         {
-            Assert.True(IsJsonNumber("12.34"));
+            var number = new Number().Match("12.34");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void TheFractionCanHaveLeadingZeros()
         {
-            Assert.True(IsJsonNumber("0.00000001"));
-            Assert.True(IsJsonNumber("10.00000001"));
+            var number = new Number().Match("0.00000001");
+            var number1 = new Number().Match("10.00000001");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
+            Assert.Equal((true, ""), (number.Success(), number1.RemainingText()));
         }
 
         [Fact]
         public void DoesNotEndWithADot()
         {
-            Assert.False(IsJsonNumber("12."));
+            var number = new Number().Match("12.");
+            Assert.Equal((true, "."), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void DoesNotHaveTwoFractionParts()
         {
-            Assert.False(IsJsonNumber("12.34.56"));
+            var number = new Number().Match("12.34.56");
+            Assert.Equal((true, ".56"), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void TheDecimalPartDoesNotAllowLetters()
         {
-            Assert.False(IsJsonNumber("12.3x"));
+            var number = new Number().Match("12.3x");
+            Assert.Equal((true, "x"), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void CanHaveAnExponent()
         {
-            Assert.True(IsJsonNumber("12e3"));
+            var number = new Number().Match("12e3");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void TheExponentCanStartWithCapitalE()
         {
-            Assert.True(IsJsonNumber("12E3"));
+            var number = new Number().Match("12E3");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void TheExponentCanHavePositive()
         {
-            Assert.True(IsJsonNumber("12e+3"));
+            var number = new Number().Match("12e+3");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void TheExponentCanBeNegative()
         {
-            Assert.True(IsJsonNumber("61e-9"));
+            var number = new Number().Match("61e-9");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void CanHaveFractionAndExponent()
         {
-            Assert.True(IsJsonNumber("12.34E3"));
+            var number = new Number().Match("12.34E3");
+            Assert.Equal((true, ""), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void TheExponentDoesNotAllowLetters()
         {
-            Assert.False(IsJsonNumber("22e3x3"));
+            var number = new Number().Match("22e3x3");
+            Assert.Equal((true, "x3"), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void DoesNotHaveTwoExponents()
         {
-            Assert.False(IsJsonNumber("22e323e33"));
+            var number = new Number().Match("22e323e33");
+            Assert.Equal((true, "e33"), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
@@ -151,41 +175,66 @@ namespace Json.Facts
             Assert.False(IsJsonNumber("22e"));
             Assert.False(IsJsonNumber("22e+"));
             Assert.False(IsJsonNumber("23E-"));
+
+            var number1 = new Number().Match("22e");
+            var number2 = new Number().Match("22e+");
+            var number3 = new Number().Match("23E-");
+            Assert.Equal((true, "e"), (number1.Success(), number1.RemainingText()));
+            Assert.Equal((true, "e+"), (number2.Success(), number2.RemainingText()));
+            Assert.Equal((true, "E-"), (number3.Success(), number3.RemainingText()));
         }
 
         [Fact]
         public void TheExponentIsAfterTheFraction()
         {
-            Assert.False(IsJsonNumber("22e3.3"));
+            var number = new Number().Match("22e3.3");
+            Assert.Equal((true, ".3"), (number.Success(), number.RemainingText()));
         }
 
         [Fact]
         public void FractionalPartStartsOnlyWithADigit()
         {
-            Assert.False(IsJsonNumber("235.a3e12"));
-            Assert.False(IsJsonNumber("665.e-8"));
-            Assert.False(IsJsonNumber("67.-9"));
+            var number1 = new Number().Match("235.a3e12");
+            var number2 = new Number().Match("665.3-8");
+            var number3 = new Number().Match("67.-9");
+            Assert.Equal((true, ".a3e12"), (number1.Success(), number1.RemainingText()));
+            Assert.Equal((true, "-8"), (number2.Success(), number2.RemainingText()));
+            Assert.Equal((true, ".-9"), (number3.Success(), number3.RemainingText()));
         }
 
         [Fact]
         public void ExponentCanBeZero()
         {
-            Assert.True(IsJsonNumber("235.3e0"));
-            Assert.True(IsJsonNumber("235.3e+0"));
-            Assert.True(IsJsonNumber("235.3e-0"));
-            Assert.True(IsJsonNumber("456.28e07"));
+            var number1 = new Number().Match("235.3e0");
+            var number2 = new Number().Match("235.3e+0");
+            var number3 = new Number().Match("235.3e-0");
+            var number4 = new Number().Match("456.28e07");
+
+            Assert.Equal((true, ""), (number1.Success(), number1.RemainingText()));
+            Assert.Equal((true, ""), (number2.Success(), number2.RemainingText()));
+            Assert.Equal((true, ""), (number3.Success(), number3.RemainingText()));
+            Assert.Equal((true, ""), (number4.Success(), number4.RemainingText()));
         }
 
         [Fact]
         public void ExponentCannotHaveMultipleSymbols()
         {
-            Assert.False(IsJsonNumber("-4986.28e--7"));
-            Assert.False(IsJsonNumber("135.28e++12"));
-            Assert.False(IsJsonNumber("9.28ee12"));
-            Assert.False(IsJsonNumber("96.28ee--901"));
-            Assert.False(IsJsonNumber("58787.28e+-22"));
-            Assert.False(IsJsonNumber("36.28e-+68"));
-            Assert.False(IsJsonNumber("6.28e+12e"));
+            var number1 = new Number().Match("-4986.28e--7");
+            var number2 = new Number().Match("135.28e++12");
+            var number3 = new Number().Match("9.28ee12");
+            var number4 = new Number().Match("96.28ee--901");
+            var number5 = new Number().Match("58787.28e+-22");
+            var number6 = new Number().Match("36.28e-+68");
+            var number7 = new Number().Match("6.28e+12e");
+
+
+            Assert.Equal((true, "e--7"), (number1.Success(), number1.RemainingText()));
+            Assert.Equal((true, "e++12"), (number2.Success(), number2.RemainingText()));
+            Assert.Equal((true, "ee12"), (number3.Success(), number3.RemainingText()));
+            Assert.Equal((true, "ee--901"), (number4.Success(), number4.RemainingText()));
+            Assert.Equal((true, "e+-22"), (number5.Success(), number5.RemainingText()));
+            Assert.Equal((true, "e-+68"), (number6.Success(), number6.RemainingText()));
+            Assert.Equal((true, "e"), (number7.Success(), number7.RemainingText()));
         }
     }
 }
---
+++ JsonValidator/Json.Start/Json.Validator/Number.cs
@@ -0,0 +1,31 @@
+﻿namespace Json
+{
+    public class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            var onenine = new Range('1', '9');
+            var digits = new Choice(new OneOrMore(digit), digit);
+            var negative = new Optional(new Character('-'));
+
+            var integer = new Choice(new Sequence(onenine, digits), digit);
+
+            var fraction = new Optional(new Sequence(new Character('.'), digits));
+
+            var exponentSymbol = new Any("eE");
+            var sign = new Optional(new Any("-+"));
+
+            var exponent = new Optional(new Sequence(exponentSymbol, sign, digits));
+
+            pattern = new Sequence(negative, integer, fraction, exponent);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
