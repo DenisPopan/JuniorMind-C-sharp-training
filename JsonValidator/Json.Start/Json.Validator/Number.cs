b8d109bff6faef061dee50d44101b604d4e4da7e
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-28 13:10:57 +0300

    Refactored Number class

--- JsonValidator/Json.Start/Json.Validator/Number.cs
+++ JsonValidator/Json.Start/Json.Validator/Number.cs
@@ -6,12 +6,12 @@
 
         public Number()
         {
-            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            var digit = new Choice(new Range('0', '9'));
             var onenine = new Range('1', '9');
-            var digits = new Choice(new OneOrMore(digit), digit);
+            var digits = new OneOrMore(digit);
             var negative = new Optional(new Character('-'));
 
-            var integer = new Choice(new Sequence(onenine, digits), digit);
+            var integer = new Sequence(negative, new Choice(new Sequence(onenine, digits), digit));
 
             var fraction = new Optional(new Sequence(new Character('.'), digits));
 
@@ -20,7 +20,7 @@
 
             var exponent = new Optional(new Sequence(exponentSymbol, sign, digits));
 
-            pattern = new Sequence(negative, integer, fraction, exponent);
+            pattern = new Sequence(integer, fraction, exponent);
         }
 
         public IMatch Match(string text)
