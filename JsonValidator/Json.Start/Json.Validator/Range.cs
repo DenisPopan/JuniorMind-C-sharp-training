2cb77c3d7bc2f79d88a3afc52a5cdac97e3ee959
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-09-29 21:57:09 +0300

    Refactored String Class

--- JsonValidator/Json.Start/Json.Validator/Range.cs
+++ JsonValidator/Json.Start/Json.Validator/Range.cs
@@ -4,16 +4,23 @@
     {
         readonly char start;
         readonly char end;
+        readonly string excepted;
 
         public Range(char start, char end)
         {
             this.start = start;
             this.end = end;
+            excepted = "\\\"";
         }
 
         public IMatch Match(string text)
         {
-            return !string.IsNullOrEmpty(text) && text[0] >= start && text[0] <= end
+            if (string.IsNullOrEmpty(text) || excepted.IndexOf(text[0]) != -1)
+            {
+                return new Match(text, false);
+            }
+
+            return text[0] >= start && text[0] <= end
                 ? new Match(text.Substring(1), true)
                 : new Match(text, false);
         }
--- JsonValidator/Json.Start/Json.Validator/String.cs
+++ JsonValidator/Json.Start/Json.Validator/String.cs
@@ -6,19 +6,18 @@
 
         public String()
         {
-            var startsWithDoubleQuotes = new Character('"');
-            var endsWithDoubleQuotes = new Character('"');
+            var quotes = new Character('"');
 
             var digit = new Range('0', '9');
             var hex = new Choice(digit, new Range('a', 'f'), new Range('A', 'F'));
             var unicode = new Sequence(new Character('u'), hex, hex, hex, hex);
             var escapeKey = new Character('\\');
             var escape = new Choice(new Any("\"\\/bfnrt"), unicode);
-            var unescapedChar = new Choice(new Range(' ', '!'), new Range('#', '['), new Range('^', (char)65535));
+            var unescapedChar = new Range(' ', char.MaxValue);
             var character = new Choice(unescapedChar, new Sequence(escapeKey, escape));
             var characters = new Many(character);
 
-            pattern = new Sequence(startsWithDoubleQuotes, characters, endsWithDoubleQuotes);
+            pattern = new Sequence(quotes, characters, quotes);
         }
 
         public IMatch Match(string text)
