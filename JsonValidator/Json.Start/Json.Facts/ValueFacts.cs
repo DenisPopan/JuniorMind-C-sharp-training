588b1145a5636810c506e4d8972b954c874d108f
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-10-01 12:45:16 +0300

    Checked if Value can be a number

--- JsonValidator/Json.Start/Json.Facts/ValueFacts.cs
+++ JsonValidator/Json.Start/Json.Facts/ValueFacts.cs
@@ -29,5 +29,23 @@ namespace Json.Facts
             Assert.Equal((true, ""), (match1.Success(), match1.RemainingText()));
             Assert.Equal((true, ""), (match2.Success(), match2.RemainingText()));
         }
+
+        [Fact]
+
+        public void ValueCanBeNumber()
+        {
+            IMatch match1 = new Value().Match("654");
+            IMatch match2 = new Value().Match("235.2353");
+            IMatch match3 = new Value().Match("9847e23554");
+            IMatch match4 = new Value().Match("234.345e66");
+            IMatch match5 = new Value().Match("654.");
+            IMatch match6 = new Value().Match("235ee");
+            Assert.Equal((true, ""), (match1.Success(), match1.RemainingText()));
+            Assert.Equal((true, ""), (match2.Success(), match2.RemainingText()));
+            Assert.Equal((true, ""), (match3.Success(), match3.RemainingText()));
+            Assert.Equal((true, ""), (match4.Success(), match4.RemainingText()));
+            Assert.Equal((true, "."), (match5.Success(), match5.RemainingText()));
+            Assert.Equal((true, "ee"), (match6.Success(), match6.RemainingText()));
+        }
     }
 }
