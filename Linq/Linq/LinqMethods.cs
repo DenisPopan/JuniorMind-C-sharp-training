e314bfc6a4e917be203c0a1fc013ae68c848abee
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-11-18 18:35:45 +0200

    Added All method
    And checked if it returns true when all elements from a source meet a given condition

--- Linq/Linq/LinqMethods.cs
+++ Linq/Linq/LinqMethods.cs
@@ -1,8 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 namespace Linq
 {
     public static class LinqMethods
     {
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            foreach (var element in source)
+            {
+                if (!predicate(element))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
--- Linq/LinqFacts/LinqMethodsFacts.cs
+++ Linq/LinqFacts/LinqMethodsFacts.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Linq;
 using Xunit;
 
 namespace LinqFacts
@@ -6,9 +8,21 @@ namespace LinqFacts
     public class LinqMethodsFacts
     {
         [Fact]
-        public void Test1()
+        public void AllMethodShouldReturnTrueIFAllElementsMeetTheGivenCondition()
         {
+            var array = new int[3];
+            array[0] = 6;
+            array[1] = 8;
+            array[2] = 10;
 
+            var array1 = new int[2];
+            array1[0] = 6;
+            array1[1] = 9;
+
+            Assert.True(LinqMethods.All<int>(array, c => c % 2 == 0));
+            Assert.False(LinqMethods.All<int>(array1, c => c % 2 == 0));
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.All<int>(null, c => c % 2 == 0));
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.All<int>(array, null));
         }
     }
 }
