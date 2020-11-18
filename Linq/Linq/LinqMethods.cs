9dd22415ab584899e8fa2979c544007298d74f77
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2020-11-18 19:12:25 +0200

    Added First method
    And checked if it returns the first element that meets the given condition

--- Linq/Linq/LinqMethods.cs
+++ Linq/Linq/LinqMethods.cs
@@ -50,5 +50,28 @@ namespace Linq
 
             return false;
         }
+
+        public static TSource First<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                {
+                    return element;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
     }
 }
--- Linq/LinqFacts/LinqMethodsFacts.cs
+++ Linq/LinqFacts/LinqMethodsFacts.cs
@@ -42,5 +42,23 @@ namespace LinqFacts
             Assert.Throws<ArgumentNullException>(() => LinqMethods.Any<int>(null, c => c % 2 == 0));
             Assert.Throws<ArgumentNullException>(() => LinqMethods.Any<int>(array, null));
         }
+
+        [Fact]
+        public void FirstMethodShouldReturnFirstElementThatMeetsTheGivenCondition()
+        {
+            var array = new int[3];
+            array[0] = 6;
+            array[1] = 7;
+            array[2] = 10;
+
+            var array1 = new int[2];
+            array1[0] = 3;
+            array1[1] = 9;
+
+            Assert.Equal(6, LinqMethods.First<int>(array, c => c % 2 == 0));
+            Assert.Throws<InvalidOperationException>(() => LinqMethods.First<int>(array1, c => c % 2 == 0));
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.First<int>(null, c => c % 2 == 0));
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.First<int>(array, null));
+        }
     }
 }
