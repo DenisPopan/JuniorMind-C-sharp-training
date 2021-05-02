a9125709394815a62fac50943b037cad8c2a95e0
DenisPopan <35832629+DenisPopan@users.noreply.github.com>
2021-05-02 20:44:53 +0300

    Added Shape class

---
+++ DiagramsProject/Shape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+
+namespace DiagramsProject
+{
+    public abstract class Shape
+    {
+        protected Shape(Graphics graphics, string text, Styling styling, PointF position)
+        {
+            Draw.EnsureIsNotNull(styling, nameof(styling));
+            Draw.EnsureIsNotNull(graphics, nameof(graphics));
+            Text = text;
+            Styling = styling;
+            Position = position;
+            var textMeasurements = graphics.MeasureString(text, styling.DrawFont);
+            const float widthAdjustment = 20;
+            const float heightAdjustment = 10;
+            TextWidth = textMeasurements.Width + widthAdjustment;
+            TextHeight = textMeasurements.Height + heightAdjustment;
+        }
+
+        public string Text { get; }
+
+        public float TextWidth { get; }
+
+        public float TextHeight { get; }
+
+        public Styling Styling { get; }
+
+        public PointF Position { get; }
+
+        public float Width { get; protected set; }
+
+        public float Height { get; protected set; }
+
+        public abstract void DrawShape();
+    }
+}
